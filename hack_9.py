2c1bf843b2e978c4052c05c0ae974596cc61ac7b
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-16 23:05:42 -0400

    feat(add) hack_python_2

--- hack_9.py
+++ hack_9.py
@@ -4,6 +4,16 @@ text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 
 
 def fn_hack_9(s):
-    result = s
-    #...
-    return result
+    result_dict = {}
+    if "foo" in s:
+        original_value = s["foo"]
+        if original_value:
+            capitalized_value = original_value[0].upper() + original_value[1:]
+        else:
+            capitalized_value = "" 
+        transformed_value = capitalized_value.replace('k', '').replace('K', '')
+        result_dict["Foo"] = transformed_value
+
+    return result_dict
+
+print(fn_hack_9({"foo":"fookziman","bar":"barziman"}))
