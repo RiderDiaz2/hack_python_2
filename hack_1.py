79cc52b0daf7a9e04515ddb7c4d031c2a06c491b
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-16 22:59:20 -0400

    feat(add) hack_python_2

--- hack_1.py
+++ hack_1.py
@@ -8,5 +8,18 @@ text: "eq" output => "eq"
 
 
 def fn_hack_1(s):
-    result = s
-    return result
+    result_parts = []
+    for i in range(0, len(s), 3):
+        chunk = s[i : i + 3]
+        
+        if len(chunk) == 3:
+            modified_chunk = f"{chunk[0]}{chunk[1].upper()}{chunk[2]}"
+            result_parts.append(modified_chunk)
+        else:
+            result_parts.append(chunk)
+            
+    return "".join(result_parts)
+
+print(fn_hack_1("fooziman"))
+print(fn_hack_1("qux"))
+print(fn_hack_1("eq"))
