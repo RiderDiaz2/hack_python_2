1a8bd9cd9aac80cbdce24bde081fb4191ef0bdfd
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-16 23:05:10 -0400

    feat(add) hack_python_2

--- hack_8.py
+++ hack_8.py
@@ -7,8 +7,22 @@ text: ["a","b","c","d"] output => ["4","3","2","1"]
 text: ["a","b"] output => ["2","1"]
 """
 
-
 def fn_hack_8(s):
-    result = s
-    #...
-    return result
+    n = len(s)
+    
+    result_elements = []
+    for i, item in enumerate(s):
+    
+        if n % 2 != 0: 
+            result_elements.append(f"{item}-{i + 1}")
+        else: 
+            result_elements.append(str(i + 1))
+            
+    result_elements.reverse()
+    
+    return result_elements
+
+print(fn_hack_8(["a","b","c","d","e"]))
+print(fn_hack_8(["a","b","c"]))
+print(fn_hack_8(["a","b","c","d"]))
+print(fn_hack_8(["a","b"]))
