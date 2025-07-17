67aea93f18d986b8af92cbe24a27a604e5b685f8
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-16 23:03:55 -0400

    feat(add) hack_python_2

--- hack_6.py
+++ hack_6.py
@@ -8,8 +8,19 @@ text: ["a","b","c","d","e"] output => ["1","-","3","-","5"]
 text: [] output => ["0"] 
 """
 
-
 def fn_hack_6(s):
-    result = s
-    #...
-    return result
+    if not s: 
+        return ["0"]
+
+    result_list = [] 
+    
+    for i, item in enumerate(s):
+        if i % 2 == 0:
+            result_list.append(str(i + 1)) 
+        else:
+            result_list.append("-")
+            
+    return result_list
+
+print(fn_hack_6(["a","b","c","d","e"]))
+print(fn_hack_6([]))  
