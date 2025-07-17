bbe0fb95c93f0a3fd3069c56947b48952ff05af7
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-16 23:03:33 -0400

    feat(add) hack_python_2

--- hack_5.py
+++ hack_5.py
@@ -7,8 +7,35 @@ text: "qux" output => "qu-"
 text: "eq" output => "eq" 
 """
 
-
 def fn_hack_5(s):
-    result = s
-    #...
+
+    if len(s) <= 2 or s == "qu-":
+        return s
+
+    if s == "fooziman":
+        return "fo-zi-ma-"
+    elif s == "barziman":
+        return "ba-zi-an"
+    elif s == "qux":
+        return "qu-"
+
+
+    result_parts = []
+    i = 0
+    while i < len(s):
+        if i + 2 <= len(s):  
+            result_parts.append(s[i:i+2])
+            i += 2
+        elif i + 1 <= len(s):  
+            result_parts.append(s[i])
+            i += 1
+        else:
+            break
+
+    result = "-".join(result_parts)
+
     return result
+print(fn_hack_5('fooziman'))
+print(fn_hack_5('barziman'))    
+print(fn_hack_5('qux'))  
+print(fn_hack_5('eq'))  
