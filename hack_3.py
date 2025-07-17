e93642d342b0baa5dcb818f26bcdc97dda8f5bfe
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-16 23:02:35 -0400

    feat(add) hack_python_2

--- hack_3.py
+++ hack_3.py
@@ -16,6 +16,29 @@ text: "qux" output => "QvX"
 
 
 def fn_hack_3(s):
-    result = s
-    #...
-    return result
+    sustituciones = {
+        'a': '@',
+        'e': '3',
+        'i': '¡',
+        'o': '0',
+        'u': 'v'
+    }
+    
+    processed_chars = []
+    n = len(s)
+    for i, char in enumerate(s):
+        lower_char = char.lower() 
+        if lower_char in sustituciones:
+            processed_chars.append(sustituciones[lower_char])
+        elif (i == 0 or i == n - 1) and lower_char.isalpha():
+            processed_chars.append(lower_char.upper())
+        else:
+            processed_chars.append(lower_char)
+            
+    return "".join(processed_chars)
+
+print(fn_hack_3('fooziman'))
+print(fn_hack_3('barziman'))    
+print(fn_hack_3('3q'))    
+print(fn_hack_3('qu'))    
+print(fn_hack_3('qux'))    
